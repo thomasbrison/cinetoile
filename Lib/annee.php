9cfb73d73d842ee04c0b9bba042ef228d6e46f2e
Thomas Brison <thomas.brison@grenoble-inp.org>
2013-07-02 23:17:11 +0200

    header + navigation bar + responsive design + bug '2A' fixed

--- Lib/annee.php
+++ Lib/annee.php
@@ -1,7 +1,7 @@
 <?php
 
 function annee_format($annee) {
-    return ($annee & $annee != "Autre") ? $annee . "A" : $annee;
+    return ($annee && $annee != "Autre") ? $annee . "A" : $annee;
 }
 
 ?>
