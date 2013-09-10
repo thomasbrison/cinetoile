5bfeeda69a157a9cb2a0dd1ebe1c56eb06a30c06
Thomas Brison <thomas.brison@grenoble-inp.org>
2013-09-10 11:27:55 +0200

    add js file for ajax calls to check logins

---
+++ js/login.js
@@ -0,0 +1,27 @@
+var loginExists = true;
+
+function checkLogin(login) {
+
+    var appendingEl = document.getElementById('loginExists');
+
+    if (!login) {
+        appendingEl.innerText = "";
+        return false;
+    }
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && (request.status === 200 || request.status === 0)) {
+            loginExists = (request.responseText === "Le login est déjà utilisé !");
+            appendingEl.innerText = request.responseText;
+            appendingEl.className = loginExists ? "exists" : "available";
+        }
+    };
+
+    request.open("GET", "membres.php/loginExists?login=" + login, true);
+    request.send(null);
+
+    return loginExists;
+
+}
