83ce7184379f5afd413d6646c56b0542dde703ce
Thomas Brison <th.brison@gmail.com>
2014-04-18 14:34:09 +0100

    message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function onload() {
     // Hide the messages that are in the message stack after the timeout that is defined by the 'js-timeout' attribute of the message element.
-    new Message().hide();
+    new MessageEl().hide();
     zoom_affiches();
     clicdroit();
 }
--- js/message.js
+++ js/message.js
@@ -1,15 +1,27 @@
-var Message = function() {
+/**
+ * Message object
+ * @param {string} content
+ * @param {int} hideTimeout
+ * @returns {Message}
+ */
+var Message = function(content, hideTimeout) {
+    this.content = content || '';
+    this.hideTimeout = hideTimeout || 3000;
+};
+
+var MessageEl = function() {
+    var el = null;
     var elTag = 'nav';
     var elId = 'message';
     var timeoutAttribute = 'js-timeout';
 
     init = function() {
-        msgEl = document.getElementById(elId);
-        if (!msgEl) {
-            msgEl = document.createElement(elTag);
-            msgEl.id = elId;
-            msgEl.innerText = '';
-            document.body.appendChild(msgEl);
+        el = document.getElementById(elId);
+        if (!el) {
+            el = document.createElement(elTag);
+            el.id = elId;
+            el.innerText = '';
+            document.body.appendChild(el);
         }
     };
 
@@ -18,42 +30,39 @@ var Message = function() {
      * @returns {undefined}
      */
     this.hide = function() {
-        var hideTimeout = parseInt(msgEl.getAttribute(timeoutAttribute));
-        msgEl.removeAttribute(timeoutAttribute);
+        var hideTimeout = parseInt(el.getAttribute(timeoutAttribute));
+        el.removeAttribute(timeoutAttribute);
         setTimeout(function() {
-            msgEl.style.display = 'none';
-            msgEl.innerText = null;
+            el.style.display = 'none';
+            el.innerText = null;
         }, hideTimeout || 3000);
     };
 
     /**
      * Add a message in the message stack with a number of milliseconds.
-     * @param {string} message The message
-     * @param {int} hideTimeout Time in ms the message should be displayed
+     * @param {Message} message The message
      * @returns {undefined}
      */
-    this.display = function(message, hideTimeout) {
-        if (!message || !message.length)
+    this.display = function(message) {
+        if (!message || !message.content.length)
             return;
-        if (msgEl.innerText && msgEl.innerText.length) {
-            msgEl.innerText += "\n";
+        if (el.innerText && el.innerText.length) {
+            el.innerText += "\n";
         }
-        msgEl.style.display = 'block';
-        msgEl.innerText += message;
-        var currentTimeout = parseInt(msgEl.getAttribute(timeoutAttribute)) || 0;
-        hideTimeout = hideTimeout || 3000;
-        msgEl.setAttribute(timeoutAttribute, currentTimeout + hideTimeout);
+        el.style.display = 'block';
+        el.innerText += message.content;
+        var currentTimeout = parseInt(el.getAttribute(timeoutAttribute)) || 0;
+        el.setAttribute(timeoutAttribute, currentTimeout + message.hideTimeout);
     };
 
     /**
      * Add a message in the message stack and automatically display the message stack.
-     * @param {string} message The message
-     * @param {int} hideTimeout Time in ms the message should be displayed
+     * @param {Message} message The message
      * @returns {undefined}
      */
-    this.displayHiding = function(message, hideTimeout) {
-        display(message, hideTimeout);
-        hide();
+    this.displayHiding = function(message) {
+        this.display(message);
+        this.hide();
     };
 
     init();
